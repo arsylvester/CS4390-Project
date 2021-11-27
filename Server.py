078fa2fb1fd4cb930f16a023b66c8d9c8057bc19
MarCodesM <74581876+MarCodesM@users.noreply.github.com>
2021-11-27 16:21:38 -0600

    Converted from py2 -> py3

--- Server.py
+++ Server.py
@@ -9,24 +9,24 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #Bind the socket to the port
 server_address = ('localhost', 10000)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
+print('starting up on %s port %s' % server_address)
 sock.bind(server_address)
 
 #listen for connections
-sock.listen(1)
+sock.listen()
 
 while True:
     #Wait for a connection
-    print >>sys.stderr, 'waiting for a connection'
+    print('waiting for a connection')
     connection, client_address = sock.accept()
 
     try:
-        print>>sys.stderr, 'connection from', client_address
+        print('connection from', client_address)
 
         #Recieve the data in small chunks and restransmit it
         while True:
-            data = connection.recv(1000)
-            print>>sys.stderr, 'received "%s"' % data
+            data = connection.recv(1024)
+            print('received "%s"' % data)
             if data:
                 dataList = data.split()
                 messageType = dataList[0]
@@ -35,16 +35,16 @@ while True:
                     returnOrder(int(dataList[1]))
                     connection.sendall("Returning data")
                 elif(messageType == "DEL"):
-                    deleteFromList(dataList[1].lower(), 16)
+                    deleteFromList(dataList[1].lower(), len(DATABASE))
                     connection.sendall(dataList[1].lower() + " has been deleted.")
                 elif(messageType == "UPDATE"):
                     updateQuantity(dataList[1].lower(), 16, int(dataList[2]))
                     connection.sendall(dataList[1] + " Has been added")
                 else:
                     #There is an error, return error
-                    connection.sendall("ERROR: Can not process Header.")
+                    connection.sendall(b"ERROR: Can not process Header.")
             else:
-                print >>sys.stderr, 'no more data from', client_address
+                print('no more data from', client_address)
                 break
     finally:
         #clean up connection
