48301f67a76ae1ca116e9d25d5a89c48e62b9bc8
MarCodesM <74581876+MarCodesM@users.noreply.github.com>
2021-11-27 13:19:02 -0600

    Updated the "user interface

--- Requester.py
+++ Requester.py
@@ -1,36 +1,62 @@
 #Implement Client requester code here. The following is example code from https://pymotw.com/2/socket/tcp.html
 import socket
 import sys
-from Data import MessageType
-from Data import Message
 
-#Testing data
-#print >>sys.stderr, 'Enum "%s"' % MessageType(2)
-#message = Message(MessageType(2), "Test string")
-#print >>sys.stderr, 'Message "%s"' % message.messageType
-#print >>sys.stderr, 'Message "%s"' % message.payload
+# These are the instructions AKA "user interface"
+def printMessage():
+    print ("\tCTP Test Case")
+
+    print ("Command GET: (GET , *Order Flag)")
+    print ("\t0 - Sort by Name")
+    print ("\t1 - Sort by Qty")
+    print ("\t2 - Sort by Date")
+    print ("\tReturns: Sorted Database")
+
+    print ("\nCommand DEL: (DEL , *Name of Item*)")
+    print ("\tReturns: Response Acknowledgment - DACK")
+
+    print ("\nCommand UPDATE: (UPDATE, *Name of Item*, *Quantity*)")
+    print ("\tReturns: Response Acknowledgment - UACK")
+
+    print ("\nExit anytime by entering 'q'")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+keepRunning = True
 
 #Connect the socket to the port where the server is listening
 server_address = ('localhost', 10000)
-print >>sys.stderr, 'connecting to %s port %s' % server_address
+print('connecting to %s port %s' % server_address)
 sock.connect(server_address)
 
-try:
-    #Send data
-    message = 'This is the message.  It will be repeated.'
-    print >>sys.stderr, 'sending "%s"' % message
-    sock.sendall(message)
+while(keepRunning):
+    print("\n")
+    printMessage()
+    message = input("Enter Command: ")
+    
+    if message == 'q':
+        keepRunning = False
 
-    #look for the response
-    amount_received = 0
-    amount_expected = len(message)
+    try:
+        #Send command
+        print('Sending "%s"' % message)
+        sock.sendall(message.encode("utf-8"))
 
-    while amount_received < amount_expected:
-        data = sock.recv(16)
-        amount_received += len(data)
-        print >>sys.stderr, 'recieved "%s"' % data
-finally:
-    print >>sys.stderr, 'closing socket'
-    sock.close()
+        #look for the response
+        amount_received = 0
+        #what is this???
+        amount_expected = len(message)
+
+        while amount_received < amount_expected:
+            data = sock.recv(16)
+            amount_received += len(data)
+            print('recieved "%s"' % data)
+    finally:
+        print('closing socket')
+        sock.close()
+
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('localhost', 10000)
+    print('connecting to %s port %s' % server_address)
+    sock.connect(server_address)
+
